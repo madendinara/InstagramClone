539a41182be61022abbd30bbceb3935a6f8c0d12
Dinara Zimanova <madendinara@gmail.com>
2021-11-17 15:07:19 -0500

    MainTabViewController created

---
+++ InstagramClone/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by Динара Зиманова on 11/17/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .darkGray
+    }
+}
