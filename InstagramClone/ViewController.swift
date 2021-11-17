3cd16c6ff735f64dda77bc50f178e46ba40b20ac
Dinara Zimanova <madendinara@gmail.com>
2021-11-17 14:55:20 -0500

    MVVM added

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Динара Зиманова on 11/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
